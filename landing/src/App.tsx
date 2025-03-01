c1a4b26ebd54fff54bc942674a32f1d0ffe18e91
Ilya Babikov <ilya.babikov@gmail.com>
2025-03-01 20:54:26 +0500

    Добавить модальное окно входа в компонент Header

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Header } from './components/Header';
+import { Header } from './components/Header/Header';
 
 // Импорт страниц
 import HomePage from './pages/home';
